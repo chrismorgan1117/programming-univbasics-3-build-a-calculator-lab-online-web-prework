bcd5937bbbe9547d6c039b67c26ba1d5d72369ca
Christopher Morgan <cwm111794@gmail.com>
2019-11-22 05:53:47 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -5,15 +5,21 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-  sub = 
-
+  subt = num1 - num2
+  puts subt 
+  subt
 end
 
 def division(num1, num2)
-
+  div = num1 / num2
+  puts div 
+  div 
 end
 
 def multiplication(num1, num2)
+  product = num1 * num2
+  puts product
+  product
 
 end
 
